e422c33d61e95435179ec02cc340a2835f63e76d
Itai Tenenbaum <taitulism@gmail.com>
2017-04-16 05:30:55 +0300

    add client index.html file and 404 handling

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const $path   = require('path');
 const express = require('express');
 
 const app = express();
 
+const clientIndexHtmlFile = $path.join(__dirname, '/public/html/index.html');
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.send('sababa')
+    res.sendFile(clientIndexHtmlFile);
+});
+
+app.use('*', (req, res) => {
+    res.sendStatus(404);
 });
 
 app.listen(8080, function () {

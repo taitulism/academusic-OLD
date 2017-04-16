73d3ff668a729bd61acbf37f34b34779a49b7136
Itai Tenenbaum <taitulism@gmail.com>
2017-04-16 05:17:08 +0300

    public folder

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.send('sababa')
 });
 
 app.listen(8080, function () {
-  console.log('Listening on port 8080!');
+    console.log('Listening on port 8080');
 });
